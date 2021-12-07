5e1ef7593181b5cb11ff68583b6d2168d8991add
codybuell <cody@codybuell.com>
2021-12-07 12:30:11 -0500

    Implement smarter methods for both parts

--- 2021/day-07/solution.py
+++ 2021/day-07/solution.py
@@ -18,6 +18,11 @@ crab_subs = [int(x) for x in crab_subs]
 ###########
 
 
+def sumorial(num: int) -> int:
+    # aka summation / n-th triangular number / terminal function
+    return (num * (num + 1)) // 2
+
+
 ###########
 #  parts  #
 ###########
@@ -56,10 +61,51 @@ def part2():
     return (winning_position, winning_pos_fuel)
 
 
+def part1_median_method():
+    # get the median (middle number in list)
+    sorted_copy = crab_subs.copy()
+    sorted_copy.sort()
+    if (len(sorted_copy) % 2) == 0:
+        # we don't have a middle number so we need to average the two around the center
+        lower = int(len(sorted_copy) / 2)
+        upper = lower + 1
+        median = (sorted_copy[lower] + sorted_copy[upper]) / 2
+    else:
+        median = sorted_copy[int(len(sorted_copy))]
+
+    # find amount of fuel spent for each sub to get to median
+    fuel_spent = 0
+    for i in crab_subs:
+        fuel_spent += abs(median - i)
+
+    return(int(median), int(fuel_spent))
+
+
+def part2_mean_method():
+    # get mean (average of numbers in list)
+    total = 0
+    for i in crab_subs:
+        total += i
+    avg = int(total / len(crab_subs))
+
+    fuel_spent = 0
+    for i in crab_subs:
+        # sumorial the difference between current pos and target (avg)
+        fuel_spent += sumorial(abs(avg - i))
+        # alt method using sum() and range()
+        # fuel_spent += sum(range(abs(avg - i) + 1))
+
+    return(avg, int(fuel_spent))
+
+
 ##########
 #  main  #
 ##########
-
 if __name__ == "__main__":
-    print('Part 1:', part1())
-    print('Part 2:', part2())
+    # brute force (1 min on m1 mac)
+    # print('Part 1:', part1())
+    # print('Part 2:', part2())
+
+    # smart way ()
+    print('Part 1:', part1_median_method())
+    print('Part 2:', part2_mean_method())
